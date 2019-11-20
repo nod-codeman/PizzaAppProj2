5713ab8ff9d20e889c9349847c2a0a2a9e1895eb
nichmihai <nichmihai4@gmail.com>
2019-11-19 21:18:20 -0500

    PizzaRepos

---
+++ Repository/IPizzarepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VMANpizza.Models;
+
+namespace VMANpizza.Repository
+{
+    interface IPizzarepo
+    {
+        Task<List<Pizza>> Get();
+        Task<Pizza> Get(int id);
+
+        Task<bool> Create(Pizza pizza);
+        Task<bool> Edit(int id, Pizza pizza);
+        Task<bool> Delete(int id);
+        bool PizzaExists(int id);
+    }
+}
---
+++ Repository/PizzaRepo.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VMANpizza.Models;
+
+namespace VMANpizza.Repository
+{
+    public class PizzaRepo : IPizzarepo
+    {
+        private AppDbContext _context;
+        public PizzaRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Create(Pizza pizza)
+        {
+            _context.Pizzas.Add(pizza);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var pizza = _context.Pizzas.Find(id);
+            _context.Pizzas.Remove(pizza);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> Edit(int id, Pizza pizza)
+        {
+            _context.Pizzas.Update(pizza);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<List<Pizza>> Get()
+        {
+            var pizzas = await _context.Pizzas.ToListAsync();
+            return pizzas;
+        }
+
+        public async Task<Pizza> Get(int id)
+        {
+           var pizza = await _context.Pizzas.FirstOrDefaultAsync(p => p.Id == id);
+
+            return pizza; 
+        }
+
+        public bool PizzaExists(int id)
+        {
+
+            return _context.Pizzas.Any(p => p.Id == id); 
+        }
+    }
+}
