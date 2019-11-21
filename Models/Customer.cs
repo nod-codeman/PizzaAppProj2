8529505fc55c4d2283e803f23b7303a467009338
Nwojo Dike <nod.codeman@gmail.com>
2019-11-21 15:03:29 -0500

    customer view modified

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace VMANpizza.Models
     public class Customer
     {
         public int ID { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
