6c72ffa40c5243310f8ac38c7310b2e3710f5f8b
abcgithubnow <aarcon33@gmail.com>
2019-11-21 08:36:32 -0500

    Serv component getPizza

---
+++ PizzaAppProj2/src/app/serv/serv.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-serv',
+  templateUrl: './serv.component.html',
+  styleUrls: ['./serv.component.scss']
+})
+export class ServComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getPizza(){
+    
+  }
+}
